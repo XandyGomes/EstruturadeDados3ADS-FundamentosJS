a526a6727ad585301bd01e306a571abce2ee5782
XandyGomes <x3.computadores@hotmail.com>
2024-02-22 21:46:09 -0300

    aula.22.02

---
+++ arquivos/string.js
@@ -0,0 +1,12 @@
+const facul = "28860729823" 
+
+var a, b, c, d, e
+
+a = facul.charAt(0)
+b = facul.charAt(1)
+c = facul.charAt(2)
+d = facul.charAt(3)
+e = facul.charAt(4)
+
+document.getElementById("root").innerHTML = a + b + c + d +e
+
